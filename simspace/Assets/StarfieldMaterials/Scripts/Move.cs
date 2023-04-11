6c1d56e3db7f20df416c7d302aada9a52a1d9b63
Barbara <bbojanowska@edu.cdv.pl>
2023-04-11 20:38:41 +0200

    tło

---
+++ simspace/Assets/StarfieldMaterials/Scripts/Move.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine; 
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+    float Target;
+	
+
+	void Update()
+	{
+        Target += Time.deltaTime / 125;
+
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, Target), 0.05f);
+
+	}
+}
